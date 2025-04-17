118c43bbed599c4f05823b889f9008e151f1ea67
ajithks98086 <ajithks98086@gmail.com>
2025-04-17 07:44:41 +0000

    Fix input.

--- frontend/src/components/users/Register.tsx
+++ frontend/src/components/users/Register.tsx
@@ -89,6 +89,7 @@ const Register: React.FC = () => {
               <input
                 type="password"
                 id="password"
+                autoComplete="new-password"
                 className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-blue-500"
                 {...formik.getFieldProps("password")}
               />
@@ -106,6 +107,7 @@ const Register: React.FC = () => {
               <input
                 type="password"
                 id="confirmPassword"
+                autoComplete="new-password"
                 className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-blue-500"
                 {...formik.getFieldProps("confirmPassword")}
               />
